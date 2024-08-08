239e8dac58aafa271b00c882402abfadde623e64
scfulga <146138900+scfulga@users.noreply.github.com>
2024-08-08 14:51:32 +0300

    bug fixed: can't put more dots after number, and added functionality to the backspace button

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ for(i = 0; i < children.length; i++){
     let childNum = children[i].textContent;
     children[i].setAttribute("id", `${childNum}`);
     document.getElementById(`${childNum}`).onclick = function() {
-        if(!operators.includes(childNum) && childNum !== '=' && childNum !== 'AC'){
+        if(!operators.includes(childNum) && childNum !== '=' && childNum !== 'AC' && childNum !== '⌫'){
             if(!operator){
                 firstNumber += childNum;
                 calcScreen.textContent = firstNumber;
@@ -21,34 +21,88 @@ for(i = 0; i < children.length; i++){
                 secondNumber += childNum;
                 calcScreen.textContent = secondNumber;
             }
+          updateDot();
         } 
-        
+
         if (operators.includes(childNum)){
-            if(operator && secondNumber){
-                firstNumber = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+            if(operator && operator != '/' && secondNumber){
+                let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+                calcScreen.textContent = result;
+                firstNumber = result.toString();
                 operator = '';
                 secondNumber = '';
-                calcScreen.textContent = firstNumber;
+                updateDot();
+            }
+            if(operator === '/' && secondNumber){
+                let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+                calcScreen.textContent = result.toFixed(2);
+                firstNumber = result.toFixed(2);
+                operator = '';
+                secondNumber = '';
+                updateDot();
+
             }
             operator = childNum;
             calcScreen.textContent = firstNumber + ' ' + operator;
+            updateDot(); 
         }
-        if (childNum === '='){
+        if (childNum === '=' && firstNumber && secondNumber){
             let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
             calcScreen.textContent = result;
-            firstNumber = result;
+            firstNumber = result.toString();
             secondNumber = '';
             operator = '';    
+            updateDot();
         }
+        else{
+            childNum.disabled;
+        }
+
+        if(!firstNumber && operators.includes(childNum)){
+            operator = '';
+            calcScreen.textContent = '';
+        }
+        if(firstNumber === '.'){
+            firstNumber = ''
+            calcScreen.textContent = '';
+        }
+        else if(secondNumber === '.'){
+            secondNumber = '';
+            calcScreen.textContent = '';
+        }
+
         if(childNum === 'AC'){
             firstNumber = '';
             secondNumber = '';
             operator = '';
             calcScreen.textContent = '';
+            updateDot();
+        }
+        if (childNum === '⌫'){
+            if (!operator && firstNumber){
+                firstNumber = firstNumber.slice(0, -1);
+                calcScreen.textContent = firstNumber;
+            }
+            else if (secondNumber){
+                secondNumber = secondNumber.slice(0, -1);
+                calcScreen.textContent = secondNumber; 
+            }
+            else if (operator){
+                operator = operator.slice(0, -1);
+                calcScreen.textContent = firstNumber;
+            }
+            updateDot();
         }
     }
 }
-
+    function updateDot(){
+        if ((!operator && firstNumber.includes('.')) || (operator && secondNumber.includes('.'))) {
+            document.getElementById('.').disabled = true;
+        }
+        else {
+            document.getElementById('.').disabled = false;
+        }
+}
 
 function operate(a, op, b){
     switch(op){
@@ -66,8 +120,6 @@ function operate(a, op, b){
             break;
     }
 }
-
-console.log(operate(5, '*',  5));
 
 function add(a, b){
     return a + b;
