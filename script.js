b8cdab332fb36244c9171d3a5a63239d5c4e9279
scfulga <146138900+scfulga@users.noreply.github.com>
2024-08-08 16:33:12 +0300

    organized code in script.js

--- script.js
+++ script.js
@@ -8,140 +8,136 @@ let secondNumber = '';
 let operator = '';
 let operators = ['+', '-', '*', '/'];
 
-for(i = 0; i < children.length; i++){
+for (i = 0; i < children.length; i++) {
     let childNum = children[i].textContent;
     children[i].setAttribute("id", `${childNum}`);
     document.getElementById(`${childNum}`).onclick = function() {
-        if(!operators.includes(childNum) && childNum !== '=' && childNum !== 'AC' && childNum !== '⌫'){
-            if(!operator){
-                firstNumber += childNum;
-                calcScreen.textContent = firstNumber;
-            }
-            else{
-                secondNumber += childNum;
-                calcScreen.textContent = secondNumber;
-            }
-          updateDot();
-        } 
 
-        if (operators.includes(childNum)){
-            if(operator && secondNumber){
-                let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
-                if(!operator === '/'){
-                    calcScreen.textContent = result;
-                    firstNumber = result.toString();
-                }
-                else{
-                    calcScreen.textContent = result.toFixed(2).toString();
-                    firstNumber = result.toFixed(2).toString();        
-                }
-                operator = '';
-                secondNumber = '';
-                updateDot();
-            }
-            operator = childNum;
-            calcScreen.textContent = firstNumber + ' ' + operator;
-            updateDot(); 
-        }
-        if (childNum === '=' && firstNumber && secondNumber){
-            let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
-            if(!operator === '/'){
-                calcScreen.textContent = result;
-                firstNumber = result.toString();
-            }
-            else{
-                calcScreen.textContent  = result.toFixed(2).toString();
-                firstNumber = result.toFixed(2).toString();
-            } 
-            secondNumber = '';
-            operator = '';    
-            updateDot();
+        if (!operators.includes(childNum) && childNum !== '=' && childNum !== 'AC' && childNum !== '⌫') {
+            handleDigitInput(childNum);
         }
 
-        else{
-            childNum.disabled;
+        if (operators.includes(childNum)) {
+            handleOperators(childNum);
         }
+        if (childNum === '=' && firstNumber && secondNumber) {
+            handleEqual();
+        }
+        handleOperatorsInput(childNum);
+        handleDotInput();
 
-        if(!firstNumber && operators.includes(childNum)){
-            operator = '';
-            calcScreen.textContent = '';
+        if (childNum === 'AC') {
+            ACBtn();
         }
-        if(firstNumber === '.'){
-            firstNumber = ''
-            calcScreen.textContent = '';
+        if (childNum === '⌫') {
+            backspaceBtn();
         }
-        else if(secondNumber === '.'){
-            secondNumber = '';
-            calcScreen.textContent = '';
-        }
-
-        if(childNum === 'AC'){
-            firstNumber = '';
-            secondNumber = '';
-            operator = '';
-            calcScreen.textContent = '';
-            updateDot();
-        }
-        if (childNum === '⌫'){
-            if (!operator && firstNumber){
-                firstNumber = firstNumber.slice(0, -1);
-                calcScreen.textContent = firstNumber;
-            }
-            else if (secondNumber){
-                secondNumber = secondNumber.slice(0, -1);
-                calcScreen.textContent = secondNumber; 
-            }
-            else if (operator){
-                operator = operator.slice(0, -1);
-                calcScreen.textContent = firstNumber;
-            }
-            updateDot();
-        }
-        if (secondNumber.includes('.')){
-            secondNumber = '';
-            document.getElementById('.').disabled = true
-        }
-
     }
 }
-    function updateDot(){
-        if ((!operator && firstNumber.includes('.')) || (operator && secondNumber.includes('.'))) {
-            document.getElementById('.').disabled = true;
-        }
-        else {
-            document.getElementById('.').disabled = false;
-        }
+
+function updateDot() {
+    if ((!operator && firstNumber.includes('.')) || (operator && secondNumber.includes('.'))) {
+        document.getElementById('.').disabled = true;
+    } else {
+        document.getElementById('.').disabled = false;
+    }
 }
 
-function operate(a, op, b){
-    switch(op){
+function handleDigitInput(childNum) {
+    if (!operator) {
+        firstNumber += childNum;
+        calcScreen.textContent = firstNumber;
+    } else {
+        secondNumber += childNum;
+        calcScreen.textContent = secondNumber;
+    }
+    updateDot();
+}
+
+function handleOperators(childNum) {
+    if (operator && secondNumber) {
+        let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+        limitDecimalsToTwo(result);
+        operator = '';
+        secondNumber = '';
+        updateDot();
+    }
+    operator = childNum;
+    calcScreen.textContent = firstNumber + ' ' + operator;
+    updateDot();
+}
+
+function handleEqual() {
+    let result = operate(parseFloat(firstNumber), operator, parseFloat(secondNumber));
+    limitDecimalsToTwo(result);
+    secondNumber = '';
+    operator = '';
+    updateDot();
+}
+
+function limitDecimalsToTwo(result) {
+    if (operator !== '/') {
+        calcScreen.textContent = result.toString();
+        firstNumber = result.toString();
+    } else {
+        calcScreen.textContent = result.toFixed(2).toString();
+        firstNumber = result.toFixed(2).toString();
+    }
+}
+
+function handleOperatorsInput(childNum) {
+    if (!firstNumber && operators.includes(childNum)) {
+        operator = '';
+        calcScreen.textContent = '';
+    }
+}
+
+function handleDotInput() {
+    if (firstNumber === '.') {
+        firstNumber = ''
+        calcScreen.textContent = '';
+    } else if (secondNumber === '.') {
+        secondNumber = '';
+        calcScreen.textContent = '';
+    }
+}
+
+function ACBtn() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    calcScreen.textContent = '';
+    updateDot();
+}
+
+function backspaceBtn() {
+    if (!operator && firstNumber) {
+        firstNumber = firstNumber.slice(0, -1);
+        calcScreen.textContent = firstNumber;
+    } else if (secondNumber) {
+        secondNumber = secondNumber.slice(0, -1);
+        calcScreen.textContent = secondNumber;
+    } else if (operator) {
+        operator = operator.slice(0, -1);
+        calcScreen.textContent = firstNumber;
+    }
+    updateDot();
+}
+
+function operate(a, op, b) {
+    switch (op) {
         case '+':
             return add(a, b);
-            break;
         case '-':
             return subtract(a, b);
-            break;
         case '*':
             return multiply(a, b);
-            break;
         case '/':
             return divide(a, b);
-            break;
     }
 }
 
-function add(a, b){
-    return a + b;
-}
-
-function subtract(a, b){
-    return a - b;
-}
-
-function multiply(a, b){
-    return a * b;
-}
-
-function divide(a, b){
-    return a / b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
